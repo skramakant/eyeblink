af44d4f98c151e6ecd74ee7d624ec0973eed0819
skramakant <ramakant.singh17@gmail.com>
2017-08-22 18:44:51 +0530

    initial commit

---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/CapturePhotoListener.java
@@ -0,0 +1,12 @@
+package com.google.android.gms.samples.vision.face.facetracker;
+
+/**
+ * Created by Axis_Inside on 22-08-2017.
+ */
+
+public interface CapturePhotoListener {
+
+    public void onCapture();
+
+    public void autoCapture();
+}
